9c342deaa7e2c100fdb1c0dccb37a9399ab56399
ethiofanos <estifanosmincel82@gmail.com>
2023-03-18 09:21:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints with puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
