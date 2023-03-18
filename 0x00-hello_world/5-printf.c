9222645e8b6eea68584c096df99a6bfca236caaf
ethiofanos <estifanosmincel82@gmail.com>
2023-03-18 09:37:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Write a C program that prints with printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
